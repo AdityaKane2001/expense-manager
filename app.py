b5794620671c2fe92650866d17efe0a70fecf75b
Aditya Kane <adityakane1@gmail.com>
2023-03-14 01:43:59 +0530

    testing

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def hello_world():
     return "Hello world!"
 
 
-def runner(*args, **kwargs):
-    app.run(host="0.0.0.0", debug=False)
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5000, debug=False)
